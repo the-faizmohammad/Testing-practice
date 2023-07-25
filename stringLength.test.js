47ba060f7fb7614997fcfe8a6f9af4b2073c4cae
user-faiz <email2faizm@gmail.com>
2023-07-26 04:03:20 +0530

    update package.json

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,7 +1,9 @@
 const stringLength = require('./stringLength');
+
 describe('stringLength Function Test', () => {
   test('returns correct length for a valid string', () => {
     const inputString = 'Hello, World!';
     const result = stringLength(inputString);
     expect(result).toBe(13);
   });
+});
