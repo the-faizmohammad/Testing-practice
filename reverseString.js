9ce66021de5798cfa067dc8961b9f6b5f33e793d
Faiz Mohammad <134736318+the-faizmohammad@users.noreply.github.com>
2023-07-26 12:05:24 +0530

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,1 +1,9 @@
+function reverseString(string) {
+  if (typeof string !== 'string') {
+    throw new Error('Input should be a string.');
+  }
+  return string.split('').reverse().join('');
+}
+
+module.exports = reverseString;
 
